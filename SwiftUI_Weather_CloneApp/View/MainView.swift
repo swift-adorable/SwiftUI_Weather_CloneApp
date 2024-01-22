76e32d6279516c671656e877410887929dc37645
Sensei Lee <swift.adorable@gmail.com>
2024-01-22 13:14:22 +0900

    Step #01 - 프로젝트를 구성하고 기본 리소스를 추가합니다.

---
+++ SwiftUI_Weather_CloneApp/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SwiftUI_Weather_CloneApp
+//
+//  Created by 이정호 on 2024/01/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
