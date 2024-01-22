e17bb379c737ea99c05b9296c8e6ccc508b9a002
Sensei Lee <swift.adorable@gmail.com>
2024-01-22 10:26:24 +0900

    Initial Commit

---
+++ SwiftUI_Weather_CloneApp/SwiftUI_Weather_CloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_CloneAppApp.swift
+//  SwiftUI_Weather_CloneApp
+//
+//  Created by 이정호 on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_CloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
